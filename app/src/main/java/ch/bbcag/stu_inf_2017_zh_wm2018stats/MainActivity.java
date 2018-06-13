553a53de8d7d5bafcc9427ea7652e071f09b62c8
BerkayKarahan03 <35917220+BerkayKarahan03@users.noreply.github.com>
2018-06-13 16:11:24 +0200

    Initial Commit

---
+++ app/src/main/java/ch/bbcag/stu_inf_2017_zh_wm2018stats/MainActivity.java
@@ -0,0 +1,13 @@
+package ch.bbcag.stu_inf_2017_zh_wm2018stats;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
